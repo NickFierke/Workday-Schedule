79aaf7c0bb03f80559408c86a844a27a99400ec4
Mike <nicholasfierke97@gmail.com>
2023-01-12 15:58:05 -0500

    schedule

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,31 @@
-//Header date
-var currentDate = moment().format('dddd, MMMM, YYYY');
-var nine = $("hour-9")
-var ten = $("hour-10")
-var eleven = $("hour-11")
-var twelve = $("hour-12")
-var one = $("hour-1")
-var two = $("hour-2")
-var three = $("hour-3")
-var four = $("hour-4")
-var five = $("hour-5")
-
+const button = $(".saveBtn")
 
 //Shows date in header
-$(function () {
-    currentDate;
-    $('#currentDay').text((currentDate));
+$(document).ready(function (){
+    var currentDate = moment().format('dddd, MMMM, YYYY');
+    $("#currentDay").text(currentDate)
 
-}
+    var now = moment().hour()
+    var timeBlock = $(".time-block")
+    timeBlock.each(function(){
+        var hourValue = parseInt($(this).attr("id"))
+        if (hourValue === now){
+            $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description present")
+        } else if (hourValue < now) {
+            $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description past")
+        } else {
+            $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description future")
+        }
+    })
+    button.on("click", function(event){
+        event.preventDefault()
+        var textArea = $(this).siblings(".col-md-10").val().replace(key)
+        var key = $(this).parent().attr("id")
+        localStorage.setItem(key, JSON.stringify (textArea))
+    })
 
-function getiInfoFromStorage (){
-    var info = local.Storage.getItem('info');
-    if ('info') {
-        projects = JSON.parse(info);
-        
-    } else {
-        info = []
+    for(let i = 9; i <= 17; i++) {
+        $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)))
+        console.log(i)
     }
-    return info;
-}
-
-
-
-});
+})
