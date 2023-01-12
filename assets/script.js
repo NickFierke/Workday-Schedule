349bb9085154287b0b350ae134248a5d20a4aded
Mike <nicholasfierke97@gmail.com>
2023-01-12 16:31:07 -0500

    Schedule

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 const button = $(".saveBtn")
 
 //Shows date in header
-$(document).ready(function (){
+$(document).ready(function () {
     var currentDate = moment().format('dddd, MMMM, YYYY');
     $("#currentDay").text(currentDate)
 
     var now = moment().hour()
     var timeBlock = $(".time-block")
-    timeBlock.each(function(){
+    timeBlock.each(function () {
         var hourValue = parseInt($(this).attr("id"))
-        if (hourValue === now){
+        if (hourValue === now) {
             $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description present")
         } else if (hourValue < now) {
             $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description past")
@@ -17,14 +17,14 @@ $(document).ready(function (){
             $(this).children(".col-md-10").attr("class", "col-8 col-md-10 description future")
         }
     })
-    button.on("click", function(event){
+    button.on("click", function (event) {
         event.preventDefault()
         var textArea = $(this).siblings(".col-md-10").val().replace(key)
         var key = $(this).parent().attr("id")
-        localStorage.setItem(key, JSON.stringify (textArea))
+        localStorage.setItem(key, JSON.stringify(textArea))
     })
 
-    for(let i = 9; i <= 17; i++) {
+    for (let i = 9; i <= 17; i++) {
         $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)))
         console.log(i)
     }
